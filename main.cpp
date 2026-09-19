9012a919086fda6f6366decb90fb8525ec52b181
simav07 <simonianav@ya.ru>
2026-09-19 15:10:53 +0300

    Onegin V.2
    Reading to buffer, errno, strings parser

--- main.cpp
+++ main.cpp
@@ -3,19 +3,31 @@
 #include <sys/stat.h>
 
 struct FileStat {
+    char * text_ptr = NULL;
+    char ** index = {};
     off_t sizeInBytes = 0;
+    size_t nLines = 0;
     unsigned int sizeOfElem = sizeof(char);
 };
 
 int ReadFromFile(const char filename[], char ** index, size_t max_lines, size_t *nStrings);
 
-int ReadFile(const char filename[]);
+int ReadFile(const char filename[], FileStat * fileInfo);
+
+size_t StringsParser(char * buffer, char diffElem, char ** index);
+
+size_t CountElems(const char * string, int Elem);
 
 void PrintStrings(char ** index_ptr, size_t nStrings);
+
+void PrintErrno(const char filename[], int err_inf);
 
 int CompareUp(const int a, const int b);
 int CompareAlpha(const void * Str1, const void * Str2);
 int CompareAlphaReverse(const void * Str1, const void * Str2);
+
+const int SUCCESS_READ = 0;
+const int BAD_READ     = -1;
 
 const size_t MAX_LEN_LINE = 5000;
 const size_t MAX_N_LINES = 100;
@@ -24,28 +36,96 @@ const char OneginFilename[] = "onegin.txt";
 
 int main() {
 
-    ReadFile("onegin.txt");
-    return 0;
+    FileStat fileInfo = {};
 
-    // char * index[MAX_N_LINES] = {};
-    // size_t nLinesOnegin = 0;
-    // int reading_result = ReadFromFile(OneginFilename, index, MAX_LEN_LINE, &nLinesOnegin);
-    // if (reading_result == errno) return errno;
+    int readingResult = ReadFile(OneginFilename, &fileInfo);
+    int err_inf = errno;
+    if (readingResult != SUCCESS_READ) {
+        PrintErrno(OneginFilename, err_inf);
+        return false;
+    }
     
+    printf("%s\n", fileInfo.text_ptr);
 
-    // printf("------------------STANDART---------------------\n");
-    // PrintStrings(index, nLinesOnegin);
-    // printf("--------------------------------------------------\n");
+    fileInfo.nLines = CountElems(fileInfo.text_ptr, '\n');
 
-    // printf(BOLD_YELLOW "------------------AFTER SORT----------------------\n\n" RESET);
-    // QuickSort(index, 0, nLinesOnegin-1, CompareAlpha);
-    // PrintStrings(index, nLinesOnegin);
-    // printf(BOLD_YELLOW "--------------------------------------------------\n" RESET);
+    char * IndexPtr[fileInfo.nLines] = {};
+    fileInfo.index = IndexPtr;
 
-    // printf(BOLD_CYAN "---------------AFTER REVERSE SORT-----------------\n\n" RESET);
-    // qsort(index, nLinesOnegin, sizeof(char *), CompareAlphaReverse);
-    // PrintStrings(index, nLinesOnegin);
-    // printf(BOLD_CYAN "-------------------------------------\n" RESET);
+    StringsParser(fileInfo.text_ptr, '\n', fileInfo.index);
+    printf("\nStrings = %llu\n", fileInfo.nLines);
+
+    //for (int i = 0; i < fileInfo.nLines; i++) printf("%p\n", fileInfo.index[i]);
+    //return true;
+
+    printf("------------------STANDART---------------------\n");
+    PrintStrings(fileInfo.index, fileInfo.nLines);
+    printf("--------------------------------------------------\n");
+
+    printf(BOLD_YELLOW "------------------AFTER SORT----------------------\n\n" RESET);
+    QuickSort(fileInfo.index, 0, fileInfo.nLines-1, CompareAlpha);
+    PrintStrings(fileInfo.index, fileInfo.nLines);
+    printf(BOLD_YELLOW "--------------------------------------------------\n" RESET);
+
+    printf(BOLD_CYAN "---------------AFTER REVERSE SORT-----------------\n\n" RESET);
+    qsort(fileInfo.index, fileInfo.nLines, sizeof(char *), CompareAlphaReverse);
+    PrintStrings(fileInfo.index, fileInfo.nLines);
+    printf(BOLD_CYAN "-------------------------------------\n" RESET);
+
+    return true;
+}
+
+size_t StringsParser(char * buffer, char diffElem, char ** index) {
+    
+    ASSERT(buffer);
+    ASSERT(index);
+
+    char * currPtr = buffer;
+    char * lastPtr = strchr(buffer, '\0');
+
+    size_t indexPtr = 0;
+    index[indexPtr++] = currPtr; // first string
+
+    while ((currPtr <= lastPtr) && (*currPtr != '\0') && (currPtr != NULL)) {
+
+        char * newPtr = strchr(currPtr, diffElem);
+        //printf("NewPtr: %p", newPtr);
+
+        if ((newPtr == NULL) || (newPtr > lastPtr)) {
+            //printf("break\n");
+            break;
+        }
+        currPtr = newPtr;
+        *currPtr = '\0';
+
+        index[indexPtr++] = currPtr+1;
+        currPtr++;
+    }
+
+    return indexPtr;
+}
+
+size_t CountElems(const char * string, int Elem) {
+
+    size_t count = 0;
+
+    while ((*string != '\0') && (*string != EOF)) {
+
+        if (*string == Elem) count++;
+        string++;
+    }
+
+    return count;
+}
+
+void PrintErrno(const char filename[], int err_inf) {
+
+    printf(RED "File: <%s>\n", filename);
+
+    const char * errorMessage = strerror(err_inf);
+
+    if (errorMessage) perror(errorMessage);
+    printf(RESET);
 }
 
 int CompareAlpha(const void * Str1, const void * Str2) {
@@ -137,25 +217,29 @@ int CompareUp(const int a, const int b) {
     return (a-b);
 }
 
-int ReadFile(const char filename[]) {
+int ReadFile(const char filename[], FileStat * fileInfo) {
 
     struct _stat fileStat = {};
     
     FILE * file_p = fopen(filename, "r");
 
-    if (_stat(filename, &fileStat) == -1) return 0;
+    if (!file_p) return errno;
 
-    FileStat fileInfo = {.sizeInBytes = fileStat.st_size};
+    if (_stat(filename, &fileStat) == -1) return errno;
 
-    char * buffer = (char *)calloc(fileInfo.sizeInBytes + (off_t)1, fileInfo.sizeOfElem);
+    (*fileInfo).sizeInBytes = fileStat.st_size;
 
-    size_t lastIndex = fread((void *)buffer, fileInfo.sizeOfElem, fileInfo.sizeInBytes, file_p);
+    char * buffer = (char *)calloc((*fileInfo).sizeInBytes + (off_t)1, (*fileInfo).sizeOfElem);
+    ASSERT(buffer);
+
+    size_t lastIndex = fread((void *)buffer, (*fileInfo).sizeOfElem, (*fileInfo).sizeInBytes, file_p);
     buffer[lastIndex] = '\0';
+
+    (*fileInfo).text_ptr = buffer;
 
     fclose(file_p);
 
-    free(buffer);
-    return 0;
+    return SUCCESS_READ;
 }
 
 int ReadFromFile(const char filename[], char ** index_ptr, size_t max_len, size_t *nStrings) {
@@ -183,7 +267,10 @@ void PrintStrings(char ** index_ptr, size_t nStrings) {
     ASSERT(index_ptr);
     ASSERT(*index_ptr);
 
-    for (size_t i = 0; i < nStrings; i++) {
-        printf("%s\n", index_ptr[i]);
+    for (size_t count = 0; count < nStrings; count++) {
+
+        ASSERT(index_ptr[count]);
+        printf("%s\n", index_ptr[count]);
+
     }
 }
